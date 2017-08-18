a4f801b6490504597db40288b45245d869e59d86
Jinzhu <wosmvp@gmail.com>
2017-08-18 18:32:22 +0800

    Register middleware randomly when testing

--- middlewares_test.go
+++ middlewares_test.go
@@ -1,28 +1,38 @@
 package middlewares
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCompileMiddlewares(t *testing.T) {
-	middlewares := &Middlewares{}
-	availableMiddlewares := []Middleware{{Name: "cookie"}, {Name: "flash", After: []string{"cookie"}}}
+	availableMiddlewares := []Middleware{{Name: "cookie"}, {Name: "flash", After: []string{"cookie"}}, {Name: "auth", After: []string{"flash"}}}
 
-	for _, m := range availableMiddlewares {
+	middlewares := registerMiddlewareRandomly(availableMiddlewares)
+	checkSortedMiddlewares(middlewares, []string{"cookie", "flash", "auth"}, t)
+}
+
+func registerMiddlewareRandomly(registeredMiddlewares []Middleware) *Middlewares {
+	middlewares := &Middlewares{}
+
+	for _, m := range registeredMiddlewares {
 		middlewares.Use(m)
 	}
 
-	testSortedMiddlewares(middlewares, []string{"cookie", "flash"}, t)
+	return middlewares
 }
 
-func testSortedMiddlewares(middlewares *Middlewares, sortedNames []string, t *testing.T) {
+func checkSortedMiddlewares(middlewares *Middlewares, sortedNames []string, t *testing.T) {
 	sortedMiddlewares := middlewares.sortMiddlewares()
 
 	if len(sortedMiddlewares) != len(sortedNames) {
-		t.Errorf("Length should be same, but got %v, expect %v", middlewares.String(), sortedNames)
+		t.Errorf("Length should be same, but got %v, expect %v", middlewares.String(), strings.Join(sortedNames, ", "))
+		return
 	}
 
 	for idx, middleware := range sortedMiddlewares {
 		if sortedNames[idx] != middleware.Name {
-			t.Errorf("Expected sorted middleware is %v, but got %v", sortedNames, middlewares.String())
+			t.Errorf("Expected sorted middleware is %v, but got %v", strings.Join(sortedNames, ", "), middlewares.String())
 		}
 	}
 }
