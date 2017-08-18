f07badc2835dc66f6cd6467aeafa9241c8d52f1a
Jinzhu <wosmvp@gmail.com>
2017-08-18 18:04:48 +0800

    Setup tests architecture

--- middlewares_test.go
+++ middlewares_test.go
@@ -3,4 +3,26 @@ package middlewares
 import "testing"
 
 func TestCompileMiddlewares(t *testing.T) {
+	middlewares := &Middlewares{}
+	availableMiddlewares := []Middleware{{Name: "cookie"}, {Name: "flash", After: []string{"cookie"}}}
+
+	for _, m := range availableMiddlewares {
+		middlewares.Use(m)
+	}
+
+	testSortedMiddlewares(middlewares, []string{"cookie", "flash"}, t)
+}
+
+func testSortedMiddlewares(middlewares *Middlewares, sortedNames []string, t *testing.T) {
+	sortedMiddlewares := middlewares.sortMiddlewares()
+
+	if len(sortedMiddlewares) != len(sortedNames) {
+		t.Errorf("Length should be same, but got %v, expect %v", middlewares.String(), sortedNames)
+	}
+
+	for idx, middleware := range sortedMiddlewares {
+		if sortedNames[idx] != middleware.Name {
+			t.Errorf("Expected sorted middleware is %v, but got %v", sortedNames, middlewares.String())
+		}
+	}
 }
