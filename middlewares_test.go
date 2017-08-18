b956114c8d7aaeaecc67d35035977601fba49665
Jinzhu <wosmvp@gmail.com>
2017-08-18 18:43:16 +0800

    Update tests

--- middlewares_test.go
+++ middlewares_test.go
@@ -1,18 +1,30 @@
 package middlewares
 
 import (
+	"math/rand"
+	"sort"
 	"strings"
 	"testing"
+	"time"
 )
 
-func TestCompileMiddlewares(t *testing.T) {
-	availableMiddlewares := []Middleware{{Name: "cookie"}, {Name: "flash", After: []string{"cookie"}}, {Name: "auth", After: []string{"flash"}}}
+func registerMiddlewareRandomly(registeredMiddlewares []Middleware) *Middlewares {
+	middlewares := &Middlewares{}
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
 
-	middlewares := registerMiddlewareRandomly(availableMiddlewares)
-	checkSortedMiddlewares(middlewares, []string{"cookie", "flash", "auth"}, t)
+	sort.Slice(registeredMiddlewares, func(i, j int) bool {
+		return r.Intn(100)%2 == 1
+	})
+
+	for _, m := range registeredMiddlewares {
+		middlewares.Use(m)
+	}
+
+	return middlewares
 }
 
-func registerMiddlewareRandomly(registeredMiddlewares []Middleware) *Middlewares {
+func registerMiddleware(registeredMiddlewares []Middleware) *Middlewares {
 	middlewares := &Middlewares{}
 
 	for _, m := range registeredMiddlewares {
@@ -35,4 +47,19 @@ func checkSortedMiddlewares(middlewares *Middlewares, sortedNames []string, t *t
 			t.Errorf("Expected sorted middleware is %v, but got %v", strings.Join(sortedNames, ", "), middlewares.String())
 		}
 	}
+}
+
+func TestCompileMiddlewares(t *testing.T) {
+	availableMiddlewares := []Middleware{{Name: "cookie"}, {Name: "flash", After: []string{"cookie"}}, {Name: "auth", Before: []string{"flash"}}}
+
+	middlewares := registerMiddlewareRandomly(availableMiddlewares)
+	checkSortedMiddlewares(middlewares, []string{"cookie", "flash", "auth"}, t)
+}
+
+func TestConflictingMiddlewares(t *testing.T) {
+	t.Skipf("conflicting middlewares")
+}
+
+func TestMiddlewaresWithRequires(t *testing.T) {
+	t.Skipf("conflicting middlewares")
 }
